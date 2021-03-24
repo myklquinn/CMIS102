10910dc3ceca8322ac3689f09d38197c53c99503
myklquinn <80681768+myklquinn@users.noreply.github.com>
2021-03-24 06:58:10 +0100

    funtion with rounding

--- quinn_assignment1.py
+++ quinn_assignment1.py
@@ -1,17 +1,30 @@
 #define constants
+from unicodedata import decimal
+
 HOURLY_RATE = 15
 COMMISSION_RATE = 0.1
 
+#define function to error check if value entered is a number
+def isNumber(message):
+    try:
+        getInput = float(input(message))
+    except ValueError:
+        print("Please enter a numerical value ")
+        getInput = float(input(message))
+    return getInput
+
+
 #input hours worked and weekly sales amount
-hoursWorked = eval(input("Hours worked "))
-weeklySales = eval(input("Weekly sales "))
+hoursWorked = isNumber("Hours worked ")
+weeklySales = isNumber("Weekly sales ")
 
 #calculate hourly pay, commission pay and total pay
 hourlyPay = hoursWorked * HOURLY_RATE
 commissionPay = weeklySales * COMMISSION_RATE
 totalPay = hourlyPay + commissionPay
 
-formatPay = "{:.2f}".format(totalPay)
+#round pay to nearest cent
+roundedPay = "{:.2f}".format(round(totalPay,2))
 
 #output total pay
-print("This week you earned $" + str(formatPay))
+print("This week you earned $" + str(roundedPay))
